38b9275f27f8af7b47f275b00da54f04ff53118a
Edward Miller <edmiller.me@gmail.com>
2021-06-15 22:04:21 +1200

    Added more colors for tasks 🌈

--- src/util/randomColor.js
+++ src/util/randomColor.js
@@ -1,5 +1,15 @@
 export const randomColor = () => {
-  const colors = ["#C2DEE5", "#F2F9AD", "#9ED2C0", "#DCC0EC", "#FF9990"];
+  const colors = [
+    "#C2DEE5",
+    "#F2F9AD",
+    "#9ED2C0",
+    "#DCC0EC",
+    "#FF9990",
+    "#F29877",
+    "#D97904",
+    "#48A1D9",
+    "#AD6CD9",
+  ];
   const randomIndex = Math.floor(Math.random() * colors.length);
   return colors[randomIndex];
 };
